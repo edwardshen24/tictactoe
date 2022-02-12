c0048c2129264068181821af96f7be68d89f7b04
Edward Shen <edwardshen24@gmail.com>
2022-02-12 15:44:45 -0800

    add data attributes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const board = document.querySelector(".gameboard");
 
 (function gameBoard(){ //module
-    let gameBoard = [0,1,2,3,4,5,6,7,8];
+    let gameBoard = [1,2,3,4,5,6,7,8,9];
     gameBoard.forEach(function (i) {
         const div = document.createElement("div");
         div.setAttribute("id", "cell");
@@ -11,19 +11,42 @@ const board = document.querySelector(".gameboard");
 })();
 
 (function displayController(){ //module
+    let previous = new Set();
+    let n = 0
     board.addEventListener('click', e => {
-        if (player1.turn){
-            e.target.innerHTML = player1.piece;
-            player1.cell.push(e.target.getAttribute('data-index'));
-            player1.turn = false;
+        let i = parseInt(e.target.getAttribute('data-index'));
+        console.log(i)
+        if (((Array.from(previous)).indexOf(i) == -1 || n == 0) && i != null){
+            if (player1.turn ){
+                e.target.innerHTML = player1.piece;
+                player1.cell.push(i);
+                player1.turn = false;
+                player2.turn = true;
+                previous.add(i);
+            }
+            else if (player2.turn ){
+                e.target.innerHTML = player2.piece;
+                player2.cell.push(i);
+                player1.turn = true;
+                player2.turn = false;
+                previous.add(i);
+            }
+            n++;
         }
-        else{
-            e.target.innerHTML = player2.piece;
-            player2.cell.push(e.target.getAttribute('data-index'));
-            player1.turn = true;
-        }
+        checkWin();
     });
 })();
+
+
+
+function checkWin(){
+    const winarr = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,6,7]]
+    let checker = (arr, target) => target.every(i => arr.includes(i));
+
+    winarr.forEach(function (n) {
+        console.log(checker(n,player1.cell))
+    });
+}
 
 const createPlayer = (piece, cell, turn) => { //factory function
     return { piece, cell, turn};
