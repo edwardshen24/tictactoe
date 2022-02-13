3a380c3382154b9dd3f7acbb88bbe162c5133453
Edward Shen <edwardshen24@gmail.com>
2022-02-13 15:30:59 -0800

    added restart

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const board = document.querySelector(".gameboard");
 
-const createPlayer = (piece, cell, turn) => { //factory function
-    return { piece, cell, turn};
+const createPlayer = (piece, cell, turn,win) => { //factory function
+    return { piece, cell, turn, win};
 };
 
-const player1 = createPlayer("x",[], true);
-const player2 = createPlayer("o",[], false);
+const createControl = (previous, n) => {
+    return {previous, n};
+};
+
+const player1 = createPlayer("x",[], true, false);
+const player2 = createPlayer("o",[], false ,false);
+const control = createControl(new Set(), 0);
 
 (function gameBoard(){ //module
     let gameBoard = [1,2,3,4,5,6,7,8,9];
@@ -18,33 +23,30 @@ const player2 = createPlayer("o",[], false);
 })();
 
 (function displayController(){ //module
-    let previous = new Set();
-    let n = 0
 
     board.addEventListener('click', e => {
         let i = parseInt(e.target.getAttribute('data-index'));
-        console.log(i)
-        if (((Array.from(previous)).indexOf(i) == -1 || n == 0) && i !== null && !isNaN(i)){
-            if (player1.turn ){
+        
+        if (((Array.from(control.previous)).indexOf(i) == -1 || n == 0) && !isNaN(i) && control.previous.size != 9 && !player1.win && !player2.win){
+            if (player1.turn){
                 e.target.innerHTML = player1.piece;
                 player1.cell.push(i);
                 player1.turn = false;
                 player2.turn = true;
-                previous.add(i);
+                control.previous.add(i);
             }
-            else if (player2.turn ){
+            else if (player2.turn){
                 e.target.innerHTML = player2.piece;
                 player2.cell.push(i);
                 player1.turn = true;
                 player2.turn = false;
-                previous.add(i);
+                control.previous.add(i);
             }
-            n++;
+            control.n++;
         }
         checkWin();
     });
 
-    
 })();
 
 function checkWin(){
@@ -55,10 +57,12 @@ function checkWin(){
     winarr.forEach(function (n) {
         if(checker(player1.cell, n)){
             display.innerHTML = "Player 1 wins."
+            player1.win = true;
             restart();
         }
         else if (checker(player2.cell, n)){
             display.innerHTML = "Player 2 wins."
+            player2.win = true;
             restart();
         }
     });
@@ -66,6 +70,29 @@ function checkWin(){
 }
 
 function restart(){
-
+    let display = document.querySelector(".display");
+    const restart = document.createElement("button");
+    restart.innerHTML = "Restart";
+    restart.setAttribute("id","restart");
+    display.appendChild(restart);
 }
 
+document.addEventListener( "click", listen );
+
+function listen(e){
+   if(e.target.id === "restart"){
+       player1.win = false;
+       player2.win = false;
+       player1.cell = [];
+       player2.cell = [];
+       player1.turn = true;
+       player2.turn = false;
+       control.previous.clear();
+       control.n = 0;
+       document.querySelector(".display").innerHTML = "";
+       const elem = document.querySelectorAll("[data-index]");
+       elem.forEach(a =>{
+           a.innerHTML = " ";
+       })
+   }
+}
