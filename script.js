90822332ab385a19da7834a37973ae4dcda12ca0
Edward Shen <edwardshen24@gmail.com>
2022-02-13 23:09:15 -0800

    add tie

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const board = document.querySelector(".gameboard");
 
 const createPlayer = (piece, cell, turn,win) => { //factory function
     return { piece, cell, turn, win};
 };
 
-const createControl = (previous, n) => {
-    return {previous, n};
+const createControl = (previous, n, tie) => {
+    return {previous, n, tie};
 };
 
 const player1 = createPlayer("x",[], true, false);
 const player2 = createPlayer("o",[], false ,false);
-const control = createControl(new Set(), 0);
+const control = createControl(new Set(), 0, false);
 
 (function gameBoard(){ //module
     let gameBoard = [1,2,3,4,5,6,7,8,9];
@@ -18,31 +17,39 @@ const control = createControl(new Set(), 0);
         const div = document.createElement("div");
         div.setAttribute("id", "cell");
         div.setAttribute("data-index", i);
-        board.appendChild(div);
+        document.querySelector(".gameboard").appendChild(div);
     });
 })();
 
-(function displayController(){ //module
 
-    board.addEventListener('click', e => {
+(function displayController(){ //module
+    document.querySelector(".display").innerHTML = "Player 1's turn.";
+    document.querySelector(".gameboard").addEventListener('click', e => {
         let i = parseInt(e.target.getAttribute('data-index'));
         
-        if (((Array.from(control.previous)).indexOf(i) == -1 || n == 0) && !isNaN(i) && control.previous.size != 9 && !player1.win && !player2.win){
+        if (((Array.from(control.previous)).indexOf(i) == -1 || control.n == 0) && !isNaN(i) && !player1.win && !player2.win){
+    
             if (player1.turn){
                 e.target.innerHTML = player1.piece;
                 player1.cell.push(i);
                 player1.turn = false;
                 player2.turn = true;
                 control.previous.add(i);
-            }
-            else if (player2.turn){
+                document.querySelector(".display").innerHTML = "Player 2's turn.";
+            } else if (player2.turn){
                 e.target.innerHTML = player2.piece;
                 player2.cell.push(i);
                 player1.turn = true;
                 player2.turn = false;
                 control.previous.add(i);
+                document.querySelector(".display").innerHTML = "Player 1's turn.";
+                
             }
             control.n++;
+        }
+        if (control.previous.size == 9){
+            control.tie = true;
+            restart();
         }
         checkWin();
     });
@@ -51,7 +58,7 @@ const control = createControl(new Set(), 0);
 
 function checkWin(){
     const winarr = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-    let checker = (arr, target) => target.every(i => arr.includes(i));
+    let checker = (a, target) => target.every(i => a.includes(i));
     let display = document.querySelector(".display");
 
     winarr.forEach(function (n) {
@@ -59,8 +66,7 @@ function checkWin(){
             display.innerHTML = "Player 1 wins."
             player1.win = true;
             restart();
-        }
-        else if (checker(player2.cell, n)){
+        } else if (checker(player2.cell, n)){
             display.innerHTML = "Player 2 wins."
             player2.win = true;
             restart();
@@ -72,6 +78,8 @@ function checkWin(){
 function restart(){
     let display = document.querySelector(".display");
     const restart = document.createElement("button");
+    if (control.tie && !player1.win && !player2.win)
+        document.querySelector(".display").innerHTML = "Tie."
     restart.innerHTML = "Restart";
     restart.setAttribute("id","restart");
     display.appendChild(restart);
@@ -91,6 +99,7 @@ function listen(e){
        control.n = 0;
        document.querySelector(".display").innerHTML = "";
        const elem = document.querySelectorAll("[data-index]");
+       control.tie = false;
        elem.forEach(a =>{
            a.innerHTML = " ";
        })
